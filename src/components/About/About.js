b35581137f845e5c6a2c0ebad26002ca1b682595
Ashking07 <ashwinkapile2002@gmail.com>
2023-02-07 10:50:16 +0530

    test

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,21 +8,11 @@ const About = () => {
           <h3 className={classes.heading}>About</h3>
           <h2 className={classes.heading1}>TechnoVision</h2>
           <p className={classes.para}>
-            Its the annual national-level techno­managerial ­cultural
-            extravaganza of the KMCT College of Engineering. Karma invites
-            innovative minds to compete with each other, share ideas and educate
-            people about new technological advancements and be a part of the
-            tech platform.
+          
           </p>
 
           <p className={classes.para}>
-            Spectacular talent stretches boundaries to inspire the participating
-            students and the rapt audience alike. A gigantic range of events
-            from advanced robotics challenges to entrepreneurship hunts
-            promising to zap up the cranial indices of participants. Skill,
-            genius and knowledge trading infused with a handful of fun, and a
-            titanic prize money sum of above half a million rupees to make Karma
-            one of the most unique tech­carnivals of the country.
+            
           </p>
         </div>
 
