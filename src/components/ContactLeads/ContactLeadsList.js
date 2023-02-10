56c802adbc22aab1d18417906ec080c46e700295
Mrudul Patel <mrudulpatel04@gmail.com>
2023-02-10 07:30:15 +0530

    bug fixes

--- src/components/ContactLeads/ContactLeadsList.js
+++ src/components/ContactLeads/ContactLeadsList.js
@@ -5,7 +5,7 @@ export const leads = [
     imgSrc:
       "https://img.freepik.com/free-vector/neon-style-coming-soon-glowing-background-design_1017-25516.jpg",
     redirectLink: "",
-    faculty: "Mrs. Sulochana Sonkamble",
+    faculty: "TBD",
     students: [
       {
         name: "Vishal Sapkal",
@@ -19,7 +19,7 @@ export const leads = [
     imgSrc:
       "https://img.freepik.com/free-vector/neon-style-coming-soon-glowing-background-design_1017-25516.jpg",
     redirectLink: "",
-    faculty: "Mrs. Sulochana Sonkamble",
+    faculty: "TBD",
     students: [
       {
         name: "Aditya Burle",
