dc4c5cb1aafb3e1e4efe02e29d31d0cec79e656c
Mrudul Patel <mrudulpatel04@gmail.com>
2023-02-11 14:19:19 +0530

    added computer dept poster

--- src/components/ContactLeads/ContactLeadsList.js
+++ src/components/ContactLeads/ContactLeadsList.js
@@ -3,7 +3,7 @@ export const leads = [
     id: 0,
     heading: "Computer",
     imgSrc:
-      "https://img.freepik.com/free-vector/neon-style-coming-soon-glowing-background-design_1017-25516.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/technovision-48d0a.appspot.com/o/computer_poster.png?alt=media&token=8afe3c19-718c-412b-a8dd-29f1bad9d2ae",
     redirectLink: "",
     faculty: "TBD",
     students: [
